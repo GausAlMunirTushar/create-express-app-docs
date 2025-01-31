4466cd298c9bfefbcb875eed5f186606aab04bc5
Gaus Al Munir Tushar <gausalmunirtushar@gmail.com>
2025-01-31 19:51:57 +0600

    Add EditorConfig, enhance homepage layout, and implement CreateAppAnimation component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,32 +1,40 @@
+import { CreateAppAnimation } from "@/components/ui/CreateAppAnimation";
+import { DynamicCodeBlock } from "fumadocs-ui/components/dynamic-codeblock";
 import Image from "next/image";
 import Link from "next/link";
 import Logo from "public/logo.svg";
 
 export default function HomePage() {
-  return (
-    <main className="container mx-auto flex flex-1 flex-col justify-center text-center">
-      <div className="flex items-center justify-center">
-        <Image
-          src={Logo}
-          alt="Create Express Appp Logo"
-          width={100}
-          height={100}
-        />
-      </div>
-      <h1 className="my-4 text-2xl font-bold">Create Express App</h1>
-      <p className="text-fd-muted-foreground mb-4">
-        Project Under Development. Stay Tuned!
-      </p>
-      <p className="text-fd-muted-foreground">
-        You can open{" "}
-        <Link
-          href="/docs"
-          className="text-fd-foreground font-semibold underline"
-        >
-          /docs
-        </Link>{" "}
-        and see the documentation.
-      </p>
-    </main>
-  );
+	return (
+		<main className="container mx-auto flex flex-1 flex-col  text-center">
+			<div className="flex items-center justify-center mt-12">
+				<Image
+					src={Logo}
+					alt="Create Express Appp Logo"
+					width={100}
+					height={100}
+				/>
+			</div>
+			<h1 className="my-4 text-7xl font-extrabold">Create Express App</h1>
+			<p className="text-fd-muted-foreground">
+				You can open{" "}
+				<Link
+					href="/docs"
+					className="text-fd-foreground font-semibold underline"
+				>
+					/docs
+				</Link>{" "}
+				to see the documentation. and see the documentation.
+			</p>{" "}
+			<div className="w-32">
+				<DynamicCodeBlock lang="bash" code="npx create-ex-app" />
+			</div>
+			*/
+			<div className="w-full">
+				<div className="w-4/12">
+					<CreateAppAnimation />
+				</div>
+			</div>
+		</main>
+	);
 }
