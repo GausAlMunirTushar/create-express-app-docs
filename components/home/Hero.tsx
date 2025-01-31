bfdec1f472917d611cc72dc585a06c6e23187633
Gaus Al Munir Tushar <gausalmunirtushar@gmail.com>
2025-01-31 21:03:41 +0600

    Add Footer component and integrate it into the homepage layout

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -16,7 +16,7 @@ const Hero = () => {
 				/>
 			</div>
 			<h1 className="my-4 text-7xl font-extrabold">Create Express App</h1>
-			<div className="flex items-center justify-center gap-3">
+			<div className="flex mt-1 items-center justify-center gap-3">
 				<Link
 					href="/docs"
 					className="text-fd-foreground font-semibold underline"
@@ -25,7 +25,7 @@ const Hero = () => {
 						Read Docs
 					</button>
 				</Link>{" "}
-				<button className="px-4 py-2 border border-gray-300 dark:border-gray-700 text-gray-800 dark:text-white rounded-lg cursor-pointer hover:bg-gray-200 transition-colors duration-300">
+				<button className="px-4 py-2 border border-gray-300 dark:border-gray-700 text-gray-800 dark:text-white rounded-lg cursor-pointer hover:bg-gray-200 transition-colors duration-300 dark:hover:bg-transparent ">
 					See Template
 				</button>
 			</div>
@@ -33,7 +33,7 @@ const Hero = () => {
 			<div className="w-56 flex  mx-auto my-3">
 				<DynamicCodeBlock lang="bash" code="npx create-ex-app my-app" />
 			</div>
-			<div className="w-full">
+			<div className="w-full flex items-center justify-center mt-2">
 				<div className="w-4/12">
 					<CreateAppAnimation />
 				</div>
--- components/ui/CreateAppAnimation.tsx
+++ components/ui/CreateAppAnimation.tsx
@@ -1,119 +1,119 @@
 "use client";
 
 import {
-  useEffect,
-  useState,
-  Fragment,
-  type ReactElement,
-  type HTMLAttributes,
+	useEffect,
+	useState,
+	Fragment,
+	type ReactElement,
+	type HTMLAttributes,
 } from "react";
 import { TerminalIcon } from "lucide-react";
 import { cn } from "@/lib/cn";
 
 export function CreateAppAnimation() {
-  const installCmd = "npx create-ex-app my-app";
-  const tickTime = 100;
-  const timeCommandEnter = installCmd.length;
-  const timeCommandRun = timeCommandEnter + 3;
-  const timeCommandEnd = timeCommandRun + 3;
-  const timeWindowOpen = timeCommandEnd + 1;
-  const timeEnd = timeWindowOpen + 1;
+	const installCmd = "npx create-ex-app my-app";
+	const tickTime = 100;
+	const timeCommandEnter = installCmd.length;
+	const timeCommandRun = timeCommandEnter + 3;
+	const timeCommandEnd = timeCommandRun + 3;
+	const timeWindowOpen = timeCommandEnd + 1;
+	const timeEnd = timeWindowOpen + 1;
 
-  const [tick, setTick] = useState(timeEnd);
+	const [tick, setTick] = useState(timeEnd);
 
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setTick((prev) => (prev >= timeEnd ? prev : prev + 1));
-    }, tickTime);
+	useEffect(() => {
+		const timer = setInterval(() => {
+			setTick((prev) => (prev >= timeEnd ? prev : prev + 1));
+		}, tickTime);
 
-    return () => {
-      clearInterval(timer);
-    };
-  }, [timeEnd]);
+		return () => {
+			clearInterval(timer);
+		};
+	}, [timeEnd]);
 
-  const lines: ReactElement[] = [];
+	const lines: ReactElement[] = [];
 
-  lines.push(
-    <span key="command_type" className="text-start">
-      {installCmd.substring(0, tick)}
-      {tick < timeCommandEnter && (
-        <div className="inline-block h-3 w-1 animate-pulse bg-white" />
-      )}
-    </span>
-  );
+	lines.push(
+		<span key="command_type" className="text-start">
+			{installCmd.substring(0, tick)}
+			{tick < timeCommandEnter && (
+				<div className="inline-block h-3 w-1 animate-pulse bg-white" />
+			)}
+		</span>
+	);
 
-  if (tick >= timeCommandEnter) {
-    lines.push(<span key="space"> </span>);
-  }
+	if (tick >= timeCommandEnter) {
+		lines.push(<span key="space"> </span>);
+	}
 
-  if (tick > timeCommandRun)
-    lines.push(
-      <div key="command_response" className="flex flex-col text-start">
-        <span className="font-bold">┌ Create Express App</span>
-        <span>│</span>
-        {tick > timeCommandRun + 1 && (
-          <>
-            <span className="font-bold">◇ Project name</span>
-            <span>│ my-app</span>
-          </>
-        )}
-        {tick > timeCommandRun + 2 && (
-          <>
-            <span>│</span>
-            <span className="font-bold">◆ Choose a Languages</span>
-          </>
-        )}
-        {tick > timeCommandRun + 3 && (
-          <>
-            <span>│ ● JavaScript</span>
-            <span>│ ○ TypeScript</span>
-          </>
-        )}
-      </div>
-    );
+	if (tick > timeCommandRun)
+		lines.push(
+			<div key="command_response" className="flex flex-col text-start">
+				<span className="font-bold">┌ Create Express App</span>
+				<span>│</span>
+				{tick > timeCommandRun + 1 && (
+					<>
+						<span className="font-bold">◇ Project name</span>
+						<span>│ my-app</span>
+					</>
+				)}
+				{tick > timeCommandRun + 2 && (
+					<>
+						<span>│</span>
+						<span className="font-bold">◆ Choose a Languages</span>
+					</>
+				)}
+				{tick > timeCommandRun + 3 && (
+					<>
+						<span>│ ● JavaScript</span>
+						<span>│ ○ TypeScript</span>
+					</>
+				)}
+			</div>
+		);
 
-  return (
-    <div
-      className="relative"
-      onMouseEnter={() => {
-        if (tick >= timeEnd) {
-          setTick(0);
-        }
-      }}
-    >
-      {tick > timeWindowOpen && (
-        <LaunchAppWindow className="absolute bottom-5 right-4 z-10 animate-in fade-in slide-in-from-top-10" />
-      )}
-      <pre className="overflow-hidden rounded-xl border text-xs">
-        <div className="flex flex-row items-center gap-2 border-b px-4 py-2">
-          <TerminalIcon className="size-4" />{" "}
-          <span className="font-bold">Terminal</span>
-          <div className="grow" />
-          <div className="size-2 rounded-full bg-red-400" />
-        </div>
-        <div className="min-h-[220px] bg-gradient-to-b from-fd-secondary [mask-image:linear-gradient(to_bottom,white,transparent)]">
-          <code className="grid p-4">{lines}</code>
-        </div>
-      </pre>
-    </div>
-  );
+	return (
+		<div
+			className="relative"
+			onMouseEnter={() => {
+				if (tick >= timeEnd) {
+					setTick(0);
+				}
+			}}
+		>
+			{tick > timeWindowOpen && (
+				<LaunchAppWindow className="absolute bottom-5 right-4 z-10 animate-in fade-in slide-in-from-top-10" />
+			)}
+			<pre className="overflow-hidden rounded-xl border text-xs">
+				<div className="flex flex-row items-center gap-2 border-b px-4 py-2">
+					<TerminalIcon className="size-4" />{" "}
+					<span className="font-bold">Terminal</span>
+					<div className="grow" />
+					<div className="size-2 rounded-full bg-green-500" />
+				</div>
+				<div className="min-h-[210px] bg-gradient-to-b from-fd-secondary [mask-image:linear-gradient(to_bottom,white,transparent)]">
+					<code className="grid p-4">{lines}</code>
+				</div>
+			</pre>
+		</div>
+	);
 }
 
 function LaunchAppWindow(
-  props: HTMLAttributes<HTMLDivElement>
+	props: HTMLAttributes<HTMLDivElement>
 ): React.ReactElement {
-  return (
-    <div
-      {...props}
-      className={cn(
-        "overflow-hidden rounded-md border bg-fd-background shadow-xl",
-        props.className
-      )}
-    >
-      <div className="relative flex h-6 flex-row items-center border-b bg-fd-muted px-4 text-xs text-fd-muted-foreground">
-        <p className="absolute inset-x-0 text-center">localhost:4000</p>
-      </div>
-      <div className="p-4 text-sm">New App launched!</div>
-    </div>
-  );
+	return (
+		<div
+			{...props}
+			className={cn(
+				"overflow-hidden rounded-md border bg-fd-background shadow-xl",
+				props.className
+			)}
+		>
+			<div className="relative flex h-6 flex-row items-center border-b bg-fd-muted px-4 text-xs text-fd-muted-foreground">
+				<p className="absolute inset-x-0 text-center">localhost:4000</p>
+			</div>
+			<div className="p-4 text-sm">New App launched!</div>
+		</div>
+	);
 }
